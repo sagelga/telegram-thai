6b6834c0297ef8a518d5cba412bb05f7a1fe8edf
Kunanon Srisuntiroj <son9912@gmail.com>
2021-12-14 15:07:18 +0000

    Add Google Analytics 4 to Website

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,15 +2,35 @@ module.exports = {
     lang: 'th-TH',
     title: 'Telegram ภาษาไทย',
     description: 'กลุ่มแปลภาษา Telegram ภาษาไทย',
-    repo: 'https://github.com/sagelga/telegram-thai',
-    repoLabel: 'GitHub',
-    docsRepo: 'https://github.com/sagelga/telegram-thai',
-    docsBranch: 'main',
-
-    editLink: true,
-    editLinkText: 'รายงานเนื้อหาไม่ถูกต้อง',
+    logo: 'https://telegram.org/img/t_logo.svg',
 
     themeConfig: {
-        logo: 'https://telegram.org/img/t_logo.svg',
+        repo: 'sagelga/telegram-thai',
+        repoLabel: 'GitHub',
+
+        docsRepo: 'https://github.com/sagelga/telegram-thai',
+        docsBranch: 'main',
+
+        editLink: true,
+        editLinkText: 'แก้ไขเนื้อหาในหน้านี้',
+        editLinkPattern: ':repo/edit/:branch/docs/:path',
+
+        backToHome: 'กลับหน้าหลัก',
+
+        lastUpdated: true,
+        lastUpdatedText: 'อัพเดทล่าสุด',
+
+        contributors: true,
+        contributorsText: 'แก้ไขโดย',
+
     },
+
+    plugins: [
+        [
+            '@vuepress/plugin-google-analytics',
+            {
+                id: 'G-M6T0MZ2093',
+            },
+        ],
+    ],
 }
