e5cfed8b2ce0027232c6817e7903ff7d6dd9c521
Kunanon Srisuntiroj <son9912@gmail.com>
2021-12-14 07:34:01 +0000

    Migrate content from Notion.so

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    lang: 'th-TH',
+    title: 'Telegram ภาษาไทย',
+    description: 'กลุ่มแปลภาษา Telegram ภาษาไทย',
+    repo: 'https://github.com/sagelga/telegram-thai',
+    repoLabel: 'GitHub',
+    docsRepo: 'https://github.com/sagelga/telegram-thai',
+    docsBranch: 'main',
+
+    editLink: true,
+    editLinkText: 'รายงานเนื้อหาไม่ถูกต้อง',
+
+    themeConfig: {
+        logo: 'https://telegram.org/img/t_logo.svg',
+    },
+}
