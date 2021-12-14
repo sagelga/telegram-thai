45153d24dde055d89ba494901763540220ce6d85
Kunanon Srisuntiroj <son9912@gmail.com>
2021-12-14 08:03:35 -0800

    2021.12.B (#2)
    * Migrate content from Notion.so
    
    * Migrate more contents to VuePress
    
    * Add Google Analytics 4 to Website
    
    * Change color accent to Telegram Blue
    
    * Add Navigation Bar to Website
    
    * Auto generate Sidebar in Website

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,16 +1,54 @@
+const fs = require("fs");
+
 module.exports = {
     lang: 'th-TH',
     title: 'Telegram ภาษาไทย',
     description: 'กลุ่มแปลภาษา Telegram ภาษาไทย',
-    repo: 'https://github.com/sagelga/telegram-thai',
-    repoLabel: 'GitHub',
-    docsRepo: 'https://github.com/sagelga/telegram-thai',
-    docsBranch: 'main',
-
-    editLink: true,
-    editLinkText: 'รายงานเนื้อหาไม่ถูกต้อง',
 
     themeConfig: {
         logo: 'https://telegram.org/img/t_logo.svg',
+
+        repo: 'sagelga/telegram-thai',
+        repoLabel: 'GitHub',
+
+        docsRepo: 'https://github.com/sagelga/telegram-thai',
+        docsBranch: 'main',
+
+        editLink: true,
+        editLinkText: 'แก้ไขเนื้อหาในหน้านี้',
+        editLinkPattern: ':repo/edit/:branch/docs/:path',
+
+        backToHome: 'กลับหน้าหลัก',
+
+        lastUpdated: true,
+        lastUpdatedText: 'อัพเดทล่าสุด',
+
+        contributors: true,
+        contributorsText: 'แก้ไขโดย',
+
+        // Navigation
+
+        navbar: require('./navbar/navbar.js'),
+        sidebar: {
+            '/contribute/': getSideBar('contribute'),
+            '/install/': getSideBar('install'),
+            '/community/': getSideBar('community'),
+        },
+
     },
+
+    plugins: [
+        [
+            '@vuepress/plugin-google-analytics',
+            {
+                id: 'G-M6T0MZ2093',
+            },
+        ],
+    ],
+}
+
+function getSideBar(root) {
+    return fs
+        .readdirSync(process.cwd() + '/docs/' + root)
+        .map(sidebar => sidebar.replace('/index.md', ''))
 }
